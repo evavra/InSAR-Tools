7ebad9f53c39c8484eb4aeab7ae8addf42dac49e
Ellis Vavra <ellisvavra@berkeley.edu>
2020-05-13 11:37:22 -0700

    now works by only specifying interferogram name; also added new functions for converting dates

--- CSS.py
+++ CSS.py
@@ -4,95 +4,56 @@ import matplotlib.pyplot as plt
 import netCDF4 as nc
 import numpy as np
 import pandas as pd
+from mpl_toolkits.axes_grid1 import ImageGrid
 from PyGMTSAR import addOrder
 from PyGMTSAR import filtIntfTable
 from PyGMTSAR import getSceneTable
 from PyGMTSAR import readBaselineTable
 from PyGMTSAR import readIntfTable
-from mpl_toolkits.axes_grid1 import ImageGrid
 
 
 # ----------- DRIVERS ----------
 def driver():
-    # ---------- Make APS estimates ----------
-
-    # SETTINGS
-    units = 'cm'
-
-    # Read metadata
+    # ---------- Settings ------------------------------------------------------------
+    #   Paths
     baselineTableFile = '/Users/ellisvavra/Desktop/LongValley/LV-InSAR/baseline_table_des.dat'
     intfTableFile = '/Users/ellisvavra/Desktop/LongValley/LV-InSAR/intf_table_NN15.dat'
+    dirPath = '/Users/ellisvavra/Desktop/LongValley/Tests/des/intf_all/'
+    intfDir = '2019225_2019237'
+    intfDates = split_intf_name(intfDir)
+
+    #   For filtering interferograms
+    Master = str2dt(['20140101', '20210101'])
+    Repeat = Master
+    TempBaseline = [0, 10**10]
+    OrbitBaseline = [-1000, 1000]
+    MeanCorr = [0.2, 1]
+    Order = [1, 15]
+
+    #   For common scene stacking (sceneTable filters)
+    Dates = intfDates
+
+    #   Other
+    units = 'cm'
+    cmap = 'Spectral'
+
+    # ---------- Make APS estimates -----------------------------------------------------
+    # Read metadata
     baselineTable = readBaselineTable(baselineTableFile)
     intfTable = readIntfTable(intfTableFile)
     intfTable = addOrder(intfTable, baselineTable)
 
-    # OPTIONAL: select interferograms to use in CSS based off of baseline, order,
-    maxOrder = 10
-    intfTable = intfTable[intfTable['Order'] <= maxOrder].reset_index(drop=True)
-
-    print('Number of interferograms to use: ' + str(len(intfTable)))
-    print()
+    # Select interferograms to use in CSS based off of baseline, order,
+    newIntfTable = filtIntfTable(intfTable, Master=Master, Repeat=Repeat, TempBaseline=TempBaseline, OrbitBaseline=OrbitBaseline, MeanCorr=MeanCorr, Order=Order)
 
     # Get individual scene information
-    sceneTable = getSceneTable(intfTable)
+    sceneTable = getSceneTable(newIntfTable)
 
     # Perform CSS
-    apsTable = css(sceneTable, intfTable, '/Users/ellisvavra/Desktop/LongValley/Tests/des/intf_all/', 2, 6)
+    apsTable = css(sceneTable, newIntfTable, dirPath, 2, 6, Dates=Dates)
 
-    # ---------- Correct intfs and plot ----------
-    # Load intf
-    intf_path = '/Users/ellisvavra/Desktop/LongValley/Tests/des/intf_all/2019225_2019231/'
-    temp = nc.Dataset(intf_path + '/unwrap.grd', 'r+', format='NETCDF4')
-    intf = np.array(temp.variables['z'])
-    temp.close()
-    intf_m = rad2m(intf, units=units)  # Convert to m
-
-    # Search apsTable for APS corrections corresponding to specified intf
-    m_aps = apsTable[['2019225_2019231' in intfList for intfList in apsTable['Masters']]]['APS'].iloc[0]
-    r_aps = apsTable[['2019225_2019231' in intfList for intfList in apsTable['Repeats']]]['APS'].iloc[0]
-
-    m_date = intfTable[['2019225_2019231' in datestr for datestr in intfTable['DateStr']]]['Master'].iloc[0]
-    r_date = intfTable[['2019225_2019231' in datestr for datestr in intfTable['DateStr']]]['Repeat'].iloc[0]
-
-    m_balance = apsTable[['2019225_2019231' in intfList for intfList in apsTable['Masters']]]['Balance'].iloc[0]
-    r_balance = apsTable[['2019225_2019231' in intfList for intfList in apsTable['Repeats']]]['Balance'].iloc[0]
-    balance = r_balance - m_balance
-
-    # Get aggregated APS
-    aps = r_aps - m_aps
-    aps_m = rad2m(aps, units=units)  # Convert to m
-
-    # Apply correction and convert to m
-    corrected = intf_m - aps_m
-
-    # Set up figure
-    fig = plt.figure(1, (13.8, 6))
-    fig.suptitle('Dates: ' + m_date.strftime('%Y/%m/%d') + ' - ' + r_date.strftime('%Y/%m/%d') + ', Order: ' + str(maxOrder) + ', Balance: ' + str(balance))
-    grid = ImageGrid(fig, 111,
-                     nrows_ncols=(1, 3),
-                     axes_pad=0.,
-                     share_all=True,
-                     aspect=1,
-                     cbar_mode='single'
-                     )
-
-    im = grid[0].imshow(intf_m)
-    im = grid[1].imshow(aps_m)
-    im = grid[2].imshow(corrected)
-    grid[0].text(len(intf_m[0]) - 30, len(intf_m) - 50, 'RMS = {}'.format(np.round(rms(intf_m), 2)),
-                 bbox=dict(facecolor='white', edgecolor='black', alpha=1))
-    # grid[1].text(len(intf_m[0]) - 30, len(intf_m) - 50, 'RMS = {}'.format(np.round(rms(aps_m), 2)),
-                 # bbox=dict(facecolor='white', edgecolor='black', alpha=1))
-    grid[2].text(len(intf_m[0]) - 30, len(intf_m) - 50, 'RMS = {}'.format(np.round(rms(corrected), 2)),
-                 bbox=dict(facecolor='white', edgecolor='black', alpha=1))
-    grid[0].set_title('Original interferogram')
-    grid[1].set_title('Estimated APS')
-    grid[2].set_title('Corrected interferogram')
-    grid[0].invert_xaxis()
-    grid[1].invert_xaxis()
-    grid[2].invert_xaxis()
-    plt.colorbar(im, label='LOS change ({})'.format(units), cax=grid.cbar_axes[0])
-    plt.show()
+    # Plot
+    plot_APS(dirPath, intfDir, intfTable, apsTable, Order, units, cmap)
 
 
 def plot_all_driver():
@@ -209,9 +170,8 @@ def plot_all_driver():
         # ax.invert_xaxis()
         # plt.show()
 
-# ---------- CALCULATIONS ----------
 
-
+# ---------- CALCULATIONS/UTILITIES ----------
 def rms(data):
     """
     Calculate root-mean-square (RMS) for Numpy array.
@@ -334,6 +294,33 @@ def m2rad(data_m, **kwargs):
     return data_rad
 
 
+def str2dt(dates_str, *kwargs):
+    """
+    Convert list (or array, series, etc.) of date strings to Python list of datetime objects
+    """
+
+    if 'format' in kwargs:  # Set format if specified
+        format = ''
+    else:  # Assume %Y%m%d as default
+        format = '%Y%m%d'
+
+    return [dt.datetime.strptime(date, format) for date in dates_str]
+
+
+def split_intf_name(intf, **kwargs):
+    """
+    Split interferogram directory name to get master and repeat dates.
+    Assumes GMTSAR julian date format.
+    """
+
+    if 'format' in kwargs:  # Set format if specified
+        format = ''
+    else:  # Assume %Y%j as default
+        format = '%Y%j'
+
+    return [dt.datetime.strptime(date, format) + dt.timedelta(days=1) for date in intf.split('_')]
+
+
 # ---------- ANALYSIS ----------
 def css(sceneTable, intfTable, pathStem, stack_min, stack_max, **kwargs):
     """
@@ -355,7 +342,26 @@ def css(sceneTable, intfTable, pathStem, stack_min, stack_max, **kwargs):
 
     Wang, K., & Fialko, Y. (2018). Observations and modeling of coseismic and postseismic deformation due to the 2015 Mw 7.8 Gorkha (Nepal) earthquake. Journal of Geophysical Research: Solid Earth, 123. https://doi.org/10.1002/2017JB014620
     """
+    # Filter table by date if specified
+    if 'DateRange' in kwargs:
+        print('Limiting scenes from {} to {}'.format(kwargs['DateRange'][0], kwargs['DateRange'][1]))
+        print()
 
+        # Select range
+        sceneTable = sceneTable[(sceneTable['Date'] >= kwargs['DateRange'][0]) & (sceneTable['Date'] <= kwargs['DateRange'][1])].reset_index(drop=True)
+
+    elif 'Dates' in kwargs:
+        print('Only correcting selected dates:')
+        for date in kwargs['Dates']:
+            print(date.strftime('%Y%m%d'))
+
+        # Select only dates in input Dates argument
+        # Use strings for datetime compatability
+        current_dates = pd.Series([date.strftime('%Y%m%d') for date in sceneTable['Date']])
+        selected_dates = [date.strftime('%Y%m%d') for date in kwargs['Dates']]
+        sceneTable = sceneTable[current_dates.isin(selected_dates)].reset_index(drop=True)
+
+    print()
     print('Starting Common Scene Stacking...')
     print()
 
@@ -422,7 +428,7 @@ def css(sceneTable, intfTable, pathStem, stack_min, stack_max, **kwargs):
             repeats.append(tempRepeats)
 
     # Retroactively assign blank APS to dates with not enough available interferograms
-    temp = nc.Dataset(pathStem + tempRepeats[0] + '/unwrap.grd', 'r+', format='NETCDF4')
+    temp = nc.Dataset(pathStem + intfTable['DateStr'].iloc[0] + '/unwrap.grd', 'r+', format='NETCDF4')
     blank_aps = np.zeros((temp.dimensions['y'].size, temp.dimensions['x'].size))
     temp.close()
 
@@ -440,9 +446,76 @@ def css(sceneTable, intfTable, pathStem, stack_min, stack_max, **kwargs):
 
 
 # ---------- PLOTS ----------
-def plot_APS():
+def plot_APS(dirPath, intfDir, intfTable, apsTable, Order, units, cmap):
+    """
+    Plot original interferogram, APS estimate, and corrected interferogram
+    """
 
-    return im
+    # ---------- Correct intfs and plot --------------------------------------------------
+    # Load intf and convert to metric units
+    temp_grid = nc.Dataset(dirPath + intfDir + '/unwrap.grd', 'r+', format='NETCDF4')
+    intf = np.array(temp_grid.variables['z'])
+    temp_grid.close()
+    intf_m = rad2m(intf, units=units)
+
+    # Get master/repeat APSs, difference, and convert to metric units, then apply correction
+    m_aps = apsTable[[intfDir in intfList for intfList in apsTable['Masters']]]['APS'].iloc[0]
+    r_aps = apsTable[[intfDir in intfList for intfList in apsTable['Repeats']]]['APS'].iloc[0]
+    aps = r_aps - m_aps
+    aps_m = rad2m(aps, units=units)
+    corrected = intf_m - aps_m
+
+    # Get master/repeat dates
+    # print('Getting dates')
+    m_date = intfTable[[intfDir in datestr for datestr in intfTable['DateStr']]]['Master'].iloc[0]
+    r_date = intfTable[[intfDir in datestr for datestr in intfTable['DateStr']]]['Repeat'].iloc[0]
+
+    # Calculate temporal balance coefficient (days)
+    # print('Getting balance')
+    m_balance = apsTable[[intfDir in intfList for intfList in apsTable['Masters']]]['Balance'].iloc[0]
+    r_balance = apsTable[[intfDir in intfList for intfList in apsTable['Repeats']]]['Balance'].iloc[0]
+    balance = r_balance - m_balance
+
+    # Get master/repeat interferogram counts
+    # print('Getting counts')
+    m_count = len(apsTable[[intfDir in intfList for intfList in apsTable['Masters']]]['Masters'].iloc[0])
+    r_count = len(apsTable[[intfDir in intfList for intfList in apsTable['Repeats']]]['Repeats'].iloc[0])
+
+    # Set up figure
+    fig = plt.figure(1, (13.8, 6))
+    grid = ImageGrid(fig, 111,
+                     nrows_ncols=(1, 3),
+                     axes_pad=0.,
+                     share_all=True,
+                     aspect=1,
+                     cbar_mode='single'
+                     )
+
+    # Plot
+    im = grid[0].imshow(intf_m, cmap=cmap)
+    im = grid[1].imshow(aps_m, cmap=cmap)
+    im = grid[2].imshow(corrected, cmap=cmap)
+
+    # Add RMS textbox to interferorams
+    grid[0].text(len(intf_m[0]) - 30, len(intf_m) - 50, 'RMS = {}'.format(np.round(rms(intf_m), 2)),
+                 bbox=dict(facecolor='white', edgecolor='black', alpha=1))
+    grid[2].text(len(intf_m[0]) - 30, len(intf_m) - 50, 'RMS = {}'.format(np.round(rms(corrected), 2)),
+                 bbox=dict(facecolor='white', edgecolor='black', alpha=1))
+
+    # Add titles
+    fig.suptitle('Dates: {} - {}, Order: {}, Masters: {}, Repeats: {}, Balance: {}'.format(m_date.strftime('%Y/%m/%d'), r_date.strftime('%Y/%m/%d'), Order[1], m_count, r_count, balance))
+    grid[0].set_title('Original interferogram')
+    grid[1].set_title('Estimated APS')
+    grid[2].set_title('Corrected interferogram')
+
+    # Flip because descending data
+    grid[0].invert_xaxis()
+    grid[1].invert_xaxis()
+    grid[2].invert_xaxis()
+
+    # Plot colorbar and show
+    plt.colorbar(im, label='LOS change ({})'.format(units), cax=grid.cbar_axes[0])
+    plt.show()
 
 
 def plot_ANC(dates, ancList):
