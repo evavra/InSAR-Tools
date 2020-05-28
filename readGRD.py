7637133ccc8e14bda1d662b01ac03eb2b1ea2ab8
Ellis Vavra <ellisvavra@berkeley.edu>
2020-05-28 16:38:59 -0700

    Now depreciated, moved to data.py

--- readGRD.py
+++ readGRD.py
@@ -1,15 +1,16 @@
 import numpy as np
 import netCDF4
 
+
 def readInSAR(filePath):
-    # Input: 
+    # Input:
     # filePath: full path to NetCDF file
     nc = netCDF4.Dataset(filePath)
-    
+
     if '_ra.' in filePath:
-        xdata = nc.variables['x']
-        ydata = nc.variables['y']
-        zdata = nc.variables['z']
+        xdata = np.array(nc.variables['x'])
+        ydata = np.array(nc.variables['y'])
+        zdata = np.flip(np.array(nc.variables['z']), 1)
 
     elif '_ll.' in filePath:
         try:
@@ -21,7 +22,7 @@ def readInSAR(filePath):
             xdata = xdata - 360
             zdata = np.flip(zdata, 0)
 
-        except KeyError:   
+        except KeyError:
             # Assume variable names have regular format
             xdata = np.array(nc.variables['x'])
             ydata = np.array(nc.variables['y'])
@@ -30,11 +31,11 @@ def readInSAR(filePath):
             xdata = xdata - 360
             zdata = np.flip(zdata, 0)
 
-    else: 
+    else:
         # Assume radar format (CANDIS default)
-        xdata = nc.variables['x']
-        ydata = nc.variables['y']
-        zdata = nc.variables['z']
+        xdata = np.array(nc.variables['x'])
+        ydata = np.array(nc.variables['y'])
+        zdata = np.flip(np.array(nc.variables['z']), 1)
 
         print('Please remember to specify radar (ra) or lat/lon (ll) in the future!')
 
