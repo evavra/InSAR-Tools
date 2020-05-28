fad4f25f6369fbdf5ffc1ce7fafb51606c0e174a
Ellis Vavra <ellisvavra@berkeley.edu>
2020-05-28 16:38:09 -0700

    consolidating data reading/writing methods here

---
+++ data.py
@@ -0,0 +1,113 @@
+import sys
+import glob
+import numpy as np
+import netCDF4 as nc
+
+"""
+Read and write InSAR data files (mostly NetCDF4 for images and ASCII for time series)
+"""
+
+
+def getFileNames(fileDir, fileType):
+    """
+    Get paths to 'fileType' files in 'fileDir' directory
+    """
+
+    # Seatches directory fileDir for files of format fileType
+    print('Searching ' + fileDir + fileType)
+    filePaths = glob.glob(fileDir + fileType)
+    filePaths.sort()
+
+    if len(filePaths) == 0:
+        print("Error! No files matching search pattern.")
+        sys.exit(1)
+
+    print("Reading data from " + str(len(filePaths)) + " files.")
+
+    return filePaths
+
+
+def readGrd(filePath):
+    """
+    Read in NetCDF grid-formattted InSAR data
+
+    INPUT:
+    filepath - full path to file
+
+    OUTPUT:
+    xdata - data contained in 'x' variable
+    ydata - data contained in 'y' variable
+    zdata - data contained in 'z' variable
+    """
+
+    grid = nc.Dataset(filePath)
+
+    if '_ra.' in filePath:
+        xdata = np.array(grid.variables['x'])
+        ydata = np.array(grid.variables['y'])
+        zdata = np.flip(grid.array(nc.variables['z']), 1)
+
+    elif '_ll.' in filePath:
+        try:
+            xdata = np.array(grid.variables['lon'])
+            ydata = np.array(grid.variables['lat'])
+            zdata = np.array(grid.variables['z'])
+
+            # Convert longitude data to Prime Meridian = 0 deg
+            xdata = xdata - 360
+            zdata = np.flip(zdata, 0)
+
+        except KeyError:
+            # Assume variable names have regular format
+            xdata = np.array(grid.variables['x'])
+            ydata = np.array(grid.variables['y'])
+            zdata = np.array(grid.variables['z'])
+
+            xdata = xdata - 360
+            zdata = np.flip(zdata, 0)
+
+    else:
+        # Assume radar format (CANDIS default)
+        xdata = np.array(grid.variables['x'])
+        ydata = np.array(grid.variables['y'])
+        zdata = np.flip(grid.array(nc.variables['z']), 1)
+
+        print('Please remember to specify radar (ra) or lat/lon (ll) in the future!')
+
+    return xdata, ydata, zdata
+
+
+def readStack(filePaths):
+    """
+    Read in stack of InSAR data
+    Uses readGrd
+
+    INPUT:
+    filePaths - list of paths to files to read
+
+    OUTPUT:
+    x - data contained in 'x' variable
+    y - data contained in 'y' variable
+    stack - stack of 'z' data
+    dates - dates of SAR acquisitions
+    """
+
+    stack = []
+    dates = []
+
+    # Read in stack of
+    for file in filePaths:
+        try:
+            # File name with ra/ll suffix (LOS_20161121_INT3_ll.grd)
+            dates.append(dt.datetime.strptime(file[-20:-12], '%Y%m%d'))
+
+        except ValueError:
+            # Default CANDIS format (# LOS_20161121_INT3.grd)
+            dates.append(dt.datetime.strptime(file[-17:-9], '%Y%m%d'))
+
+        print('Reading ' + dates[-1].strftime('%Y-%m-%d') + ' ...')
+        x, y, z = readInSAR(file)
+
+        zCube.append(z)
+
+    return x, y, stack, dates
